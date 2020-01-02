cd852419ba4839f0f31bb5ab95d60fca8c3f6046
rvieracds <rviera@codigodelsur.com>
2020-01-02 11:16:51 -0300

    add splash screen

---
+++ app/src/main/java/com/example/themoviedb/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.themoviedb
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.splash_screen.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        splash_screen_logo.setImageResource(R.mipmap.splash_screen_logo)
+
+        val SPLASH_TIME_OUT = 2000
+        val homeIntent = Intent(
+            this@SplashActivity,
+            MainActivity::class.java
+        )
+
+        Handler().postDelayed({
+            //Do some stuff here, like implement deep linking
+            startActivity(homeIntent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
+
+    }
+}
